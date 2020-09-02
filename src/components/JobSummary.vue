19cc0bf7bfba85796f5e28ceab9acf2b0973b34d
Eric Hagemeyer <erichagemeyer@gmail.com>
2020-09-02 13:45:52 -0400

    added job summaries

---
+++ src/components/JobSummary.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="job-summary">
+        <div class="job-summary__info d-flex justify-space-between mb-3">
+            <div class="job-summary__company-info">
+                <div class="job-summary__title font-weight-bold">{{ title }}</div>
+                <div class="job-summary__company">
+                    {{ company }}, <span class="font-italic">{{ location }}</span>
+                </div>
+            </div>
+            <div class="job-summary__dates font-weight-bold">
+                {{ startDate }} &mdash; {{ endDate }}
+            </div>
+        </div>
+        <div class="job-summary__responsibilities">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            default: '',
+        },
+        company: {
+            type: String,
+            default: '',
+        },
+        location: {
+            type: String,
+            default: '',
+        },
+        startDate: {
+            type: String,
+            default: '',
+        },
+        endDate: {
+            type: String,
+            default: 'Present',
+        },
+    },
+};
+</script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,31 @@
 <template>
     <v-container>
-        <h1 class="text-h1">Eric Hagemeyer</h1>
+        <h1 class="text-h3 my-6">Professional Experience</h1>
+        <job-summary
+            title="Software Developer"
+            company="Cincinnati Incorporated"
+            location="Boston, MA"
+            start-date="December 2017"
+        >
+            <ul>
+                <li>
+                    As the sole software developer for the CI Boston business unit, I worked with
+                    our General Manager to plan software projects for 2018, 2019, and 2020. All
+                    major projects were executed on schedule.
+                </li>
+            </ul>
+        </job-summary>
     </v-container>
 </template>
 
 <script>
-export default {};
+import JobSummary from '@/components/JobSummary.vue';
+
+export default {
+    components: {
+        JobSummary,
+    },
+};
 </script>
 
 <style lang="scss"></style>
