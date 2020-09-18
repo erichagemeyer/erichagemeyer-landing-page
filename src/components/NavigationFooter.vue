d95a671df53f23dc92e2deaa17f571318c5658f6
Eric Hagemeyer <erichagemeyer@gmail.com>
2020-09-18 12:27:53 -0400

    added footer to mobile menu

---
+++ src/components/NavigationFooter.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-footer absolute paddless class="white text-center">
+        <v-card flat tile width="100%" class="text-center">
+            <v-card-text>
+                <v-btn
+                    icon
+                    target="_blank"
+                    href="https://www.linkedin.com/in/eric-hagemeyer-1720539a/"
+                >
+                    <v-icon>mdi-linkedin</v-icon>
+                </v-btn>
+                <v-btn icon target="_blank" href="https://github.com/erichagemeyer">
+                    <v-icon>mdi-github</v-icon>
+                </v-btn>
+            </v-card-text>
+            <v-divider />
+            <v-card-text class="pa-1">&copy; 2020</v-card-text>
+        </v-card>
+    </v-footer>
+</template>
+
+<script>
+export default {};
+</script>
